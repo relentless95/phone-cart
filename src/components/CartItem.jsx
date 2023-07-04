eaf799bc857312b04585690d1acc24b4dc26ce73
relentless95 <76557555+relentless95@users.noreply.github.com>
2023-07-04 23:21:49 +0200

    adding the reducer function

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -9,6 +9,16 @@ const CartItem = () => {
       <div>
         <h5>title</h5>
         <span className="item-price">price</span>
+        <button className="remove-btn">remove</button>
+      </div>
+      <div>
+        <button className="amount-btn">
+          <FaChevronUp className="amount-icon" />
+        </button>
+        <span className="amount">amount</span>
+        <button className="amount-btn">
+          <FaChevronDown className="" />
+        </button>
       </div>
     </article>
   );
--- src/context/Context.jsx
+++ src/context/Context.jsx
@@ -2,14 +2,23 @@ import { useContext, useReducer, useEffect, createContext } from "react";
 // import reducer from "../reducer"
 import cartItems from "../data";
 
+import {
+  CLEAR_CART,
+  REMOVE,
+  INCREASE,
+  DECREASE,
+  LOADING,
+  DISPLAY_ITEMS,
+} from "../actions/actions";
+
 const AppContext = createContext();
-// const initialState = {
-//   loading: false,
-//   cart: new Map(cartItems.map((item) => [item.id, item])),
-// };
+const initialState = {
+  loading: false,
+  cart: new Map(cartItems.map((item) => [item.id, item])),
+};
 
 export const AppProvider = ({ children }) => {
-  const greeting ="hello"
+  const greeting = "hello";
   // const []
 
   return (
